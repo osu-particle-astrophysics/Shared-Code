6a447c74fe1f7eec34e91f03305ff6e65d2e1d97
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-01-11 17:51:10 -0500

    Update GenerateARA.cpp

--- GA/SourceFiles/GenerateARA.cpp
+++ GA/SourceFiles/GenerateARA.cpp
@@ -1,3 +1,5 @@
+// This function will generate one antenna for the ARA experiment and then call the constraint function
+
 #include <time.h>
 #include <math.h>
 #include <random>
@@ -9,3 +11,26 @@
 #include <chrono>
 #include <ctime>
 
+vector<vector<float> > Generate(int sections, int genes)
+{
+  // initialize an interesect condidtion
+  bool intersect = True;
+  for(int i=0; i<sections, i++)
+  {
+    // while the intersect condition is true, generate the side of an individual
+    while(intersect==True)
+    {
+      std::uniform_real_distribution <float> distribution_radius(0, max_radius);          // Inner Radius
+      float R = distribution_radius(generator);
+      std::uniform_real_distribution <float> distribution_length(min_length, max_length); // Length
+      float L = distribution_length(generator)
+      std::uniform_real_distribution <float> distribution_A(min_A, max_A);                // linear coefficient
+      float A = distribution_A(generator);
+      std::uniform_real_distribution <float> distribution_B(min_B, max_B);                // quadratic coefficient
+      float B = distribution_B(generator);
+      
+      // Take the individual and pass it to the constraint function and update the interesct condition
+      intersect = ConstraintARA(R, L, A, B)
+    }
+  }
+}
