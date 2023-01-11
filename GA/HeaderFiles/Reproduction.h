5bdab5bdc463b2dcaf261c74368e57d23d2ce31c
dylanwells37 <105832162+dylanwells37@users.noreply.github.com>
2023-01-11 16:20:48 -0500

    Create Reproduction.h

---
+++ GA/HeaderFiles/Reproduction.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void reproduction(vector<vector<vector<float> > > & varInput, vector<vector<vector<float> > > & varOutput, vector<float> fitness, vector<int> P_loc, vector<int> & selected, float roul_percentage, float tour_percentage, float rank_percentage, int reproduction_no, int pool_size, int elite)
