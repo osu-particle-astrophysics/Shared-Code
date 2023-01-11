1c6f03c1f6e05fee7ce2948ddf25736672e93c8c
dylanwells37 <105832162+dylanwells37@users.noreply.github.com>
2023-01-11 16:21:50 -0500

    Create Immigration.h

---
+++ GA/HeaderFiles/Immigration.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void immigration(vector<vector<vector<float> > > & varOutput, int reproduction_no, int crossover_no, float max_length, float max_radius, float max_sepration, float max_outer_radius, float max_A, float max_B)
