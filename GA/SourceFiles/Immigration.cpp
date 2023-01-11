20823c16943216d6d325e20efa5e0b7dcaca683b
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-01-11 16:29:35 -0500

    Create Immigration.cpp

---
+++ GA/SourceFiles/Immigration.cpp
@@ -0,0 +1,56 @@
+#include <time.h>
+#include <math.h>
+#include <random>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <chrono>
+#include <ctime>
+
+void immigration(vector<vector<vector<float> > > & varOutput, int reproduction_no, int crossover_no, float max_length, float max_radius, float max_seperation, float max_outer_radius, float max_A, float max_B)
+{
+  //cout << "mutation flag" << endl;
+  uniform_real_distribution<float> l_mut(min_length, max_length);
+  uniform_real_distribution<float> r_mut(0, max_radius);
+  uniform_real_distribution<float> a_mut(min_A, max_A);
+  uniform_real_distribution<float> b_mut(min_B, max_B);
+  uniform_real_distribution<float> s_mut(min_seperation, max_seperation);
+
+  for(int i=reproduction_no+crossover_no; i< varOutput.size(); i++)
+    {
+      for(int j=0; j<NSECTIONS; j++)
+	{
+	  //immigration generators
+	  varOutput[i][j][0] = r_mut(generator);
+	  varOutput[i][j][1] = l_mut(generator);
+	  varOutput[i][j][2] = a_mut(generator);
+	  varOutput[i][j][3] = b_mut(generator);
+	  varOutput[i][j][4] = s_mut(generator);
+	  // gene values
+          float R= varOutput[i][j][0]; 
+          float l= varOutput[i][j][1]; 
+          float a= varOutput[i][j][2]; 
+          float b= varOutput[i][j][3]; 
+          float end_point = (a*l*l + b*l + R); 
+          float vertex = (R - (b*b)/(4*a)); 
+          //cout << end_point << endl; 
+          //cout << vertex << endl;
+	  if(a == 0.0 && max_outer_radius > end_point && end_point >= 0.0)
+	    {
+	      j=j;
+	    }
+          else if(a != 0.0 && max_outer_radius > end_point && end_point >= 0.0 && max_outer_radius > vertex && vertex >= 0.0)
+	  {
+            j= j;
+          }
+          else{
+		//cout << "Cannot create individual!" << endl;
+            j= j-1;
+          }
+	  
+	}
+    }
+  //cout << "Immigration finished" << endl;
+}
