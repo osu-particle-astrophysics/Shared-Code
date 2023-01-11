26cdcb4ca95c77e615da1dd78669a7ca959f4bcb
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-01-11 16:22:36 -0500

    Create Rank.cpp

---
+++ GA/SourceFiles/Rank.cpp
@@ -0,0 +1,38 @@
+#include <time.h>
+#include <math.h>
+#include <random>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <chrono>
+#include <ctime>
+
+int Rank(vector<float> fitness)
+{
+  vector<float> probabilities;
+  float sum_npop=0;
+  for(int i=1; i<=fitness.size(); i++)
+    {
+      sum_npop = sum_npop + i;
+    }
+  // cout << sum_npop << endl;
+  for(int j=0; j<fitness.size(); j++)
+    {
+      probabilities.push_back((fitness.size()-j)/(sum_npop));
+      // cout << probabilities[j] << endl; 
+    }
+
+  uniform_real_distribution<float> choice(0.0, 1.0);
+  float select = choice(generator);
+
+  int x=0;
+  float probability_sum =0;
+  for (int k=0; probability_sum <= select; k++)
+    {
+      probability_sum = probability_sum + probabilities[k];
+      x=k;
+    }
+  return(x);
+}
