eab54ae0be3eab8f2dd212c7cfdc35a93df43bd7
dylanwells37 <105832162+dylanwells37@users.noreply.github.com>
2023-01-18 16:21:40 -0500

    Update ConstraintPUEO.cpp

--- GA/SourceFiles/ConstraintPUEO.cpp
+++ GA/SourceFiles/ConstraintPUEO.cpp
@@ -9,3 +9,33 @@
 #include <chrono>
 #include <ctime>
 
+bool ConstraintPUEO(vector<vector<float> > varInput)
+{
+  bool intersect = true;
+  float max_S = 50;
+  float max_H = 50;
+  // these indicies will change depending on how the individual is formatted.
+  float S = varInput[0];
+  float H = varInput[1];
+  float x_0 = varInput[2];
+  float x_f = varInput[3];
+  float y_0 = varInput[4];
+  float y_f = varInput[5];
+  float z_f = varInput[6];
+  float beta = varInput[7];
+  
+  if(S > max_S || H > max_H){
+    intersect = true;
+  }else if(x_0 < 0 || x_0 > x_f){
+    intersect = true;
+  }else if(y_0 < 0 || y_0 > z_f){
+    intersect = true;
+  }else if(z_f < 0 || z_f > H){
+    intersect = true;
+  }else if((4/30)*z_f > beta || (7*z_f) < beta){
+    intersect = true;
+  }else{
+    intersect = false;
+  }
+  return intersect;
+}
