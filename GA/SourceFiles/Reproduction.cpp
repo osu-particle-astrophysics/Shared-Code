a2cb7ae2cbfbcaa984771a5477465ca58817ddb3
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-01-18 17:54:08 -0500

    Update Reproduction.cpp

--- GA/SourceFiles/Reproduction.cpp
+++ GA/SourceFiles/Reproduction.cpp
@@ -11,85 +11,18 @@
 
 void Reproduction(vector<vector<vector<float> > > & varInput, vector<vector<vector<float> > > & varOutput, vector<float> fitness, vector<int> P_loc, vector<int> & selected, float roul_percentage, float tour_percentage, float rank_percentage, int reproduction_no, int pool_size, int elite)
 {
-
-  if(elite == 1)
+    vector<int> locations = [];
+    Selection(Reproduction_no, fitness, Roul_no, Rank_no, Tour_no, Pool);
+    
+    for(int i=0; i<Reproduction_no; i++)
     {
-      reproduction_no = reproduction_no - elite;
-      int E_select = Elite(fitness);
-      for(int i= 0; i<NSECTIONS; i++)
+    	selected.push_back(varInput[locations[i]);
+	for(int j=0; j<Sections; j++)
 	{
-	  for(int j=0; j<NVARS; j++)
+	    for(int k=0; k<Parameters; k++)
 	    {
-	      varOutput[0][i][j] = varInput[E_select][i][j];
+	    	varOutput[i][j][k] = varInput[locations[i]][j][k];
 	    }
-	  
 	}
-
-      selected.push_back(P_loc[E_select]);
-    }
-
-  int roul_no = roul_percentage*reproduction_no;
-  int tour_no = tour_percentage*reproduction_no;
-  int rank_no = rank_percentage*reproduction_no;
-
-  if(reproduction_no == 1)
-    {
-      roul_no = 1; 
-    }
-
-  while(roul_no + tour_no + rank_no < reproduction_no)
-    {
-      tour_no += 1;
-    }
-
-  if(roul_no > 0)
-    {
-      for(int i=elite; i<roul_no + elite; i++)
-	{
-	  int r_select = new_roulette(fitness); 
-	  for(int j=0; j<NSECTIONS; j++)
-	    {
-	      for(int k=0; k<NVARS; k++)
-		{
-		  varOutput[i][j][k] = varInput[r_select][j][k];
-		}
-	    
-	    }
-	  selected.push_back(P_loc[r_select]);
-	}
-    }
-  if(tour_no > 0)
-    {
-      for(int x=roul_no+elite; x<tour_no+roul_no+elite; x++)
-	{
-	  int t_select = new_tournament(fitness, pool_size);
-	  for(int y=0; y<NSECTIONS; y++)
-	    {
-	      for(int z=0; z<NVARS; z++)
-		{
-		  varOutput[x][y][z] = varInput[t_select][y][z];
-		}
-	    }
-	  selected.push_back(P_loc[t_select]);
-	}
-    }
-  if(rank_no > 0)
-    {
-       for(int r=tour_no+roul_no+elite; r<reproduction_no+elite; r++)
-	 {
-	   int k_select = Rank(fitness);
-           for(int y=0; y<NSECTIONS; y++)
-            {
-              for(int z=0; z<NVARS; z++)
-                {
-                  varOutput[r][y][z] = varInput[k_select][y][z];
-                }
-            }
-	   selected.push_back(P_loc[k_select]);
-	 }
-    }
-  if(elite == 1)
-    {
-      reproduction_no = reproduction_no+elite; 
     }
 }
