733160be48bd5046f036b6ff54fe5fc5ef87cce2
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-02-01 17:58:59 -0500

    Update New_GA.cpp

--- GA/SourceFiles/New_GA.cpp
+++ GA/SourceFiles/New_GA.cpp
@@ -21,13 +21,79 @@
 
 using namespace std;
 
+// GLOBAL CONSTANTS
+
+int seed = time(NULL);
+default_random_engine generator(seed);
+int generation;
+int population;
+int sections;
+int genes;
+int reproduction_no;
+int crossover_no;
+int mutation_rate;
+int sigma; 
+
+
 int main()
 {
-  // make vectors here
-  //
-  // Call functions here
-  // if generation zero:
-  //    Initalize(ARA/PUEO) 
+  cout << "Genetic Algorithm initialized" << endl;
+  
+  // ARGUMENTS (read in all arguments that determine what functions get run) 
+  str design = ARA;  string(argv[1]); // read in ARA or PUEO
+  generation = atoi(argv[2]);
+  population = atoi(argv[3]); // read in : atoi(argv[x])
+  rank_no = atoi(argv[4]);
+  roulette_no = atoi(argv[5]);
+  tournament_no = atoi(argv[6]);
+  reproduction_no = atoi(argv[7]);
+  crossover_no = atoi(argv[8]);
+  mutation_rate = atoi(argv[9]);
+  sigma = atoi(argv[10]);
+  
+  
+  // VECTORS
+  vector<int> P_loc (population); // Parent locations vector
+  vector<vector<vector<float>>> varInput (population,vector<vector<float> >(sections,vector <float>(genes, 0.0f))); // stores all input antennas
+  vector<vector<vector<float>>> varOutput (population,vector<vector<float> >(sections,vector <float>(genes, 0.0f))); // stores all output antennas
+  vector<float> fitness (population, 0.0f); // stores fitness score
+  vector<int> selected = [];
+  
+  // FUNCTION CALLS
+  
+  // Generation zero functions
+  if (generation == 0)
+  {
+    // if ARA, create ARA atennas
+    if (design == "ARA")
+    {
+      // determine sections and genes for ara
+      sections = 2;
+      genes = 4;
+      
+      // generate each individual
+      for(int i=0; i<NPOP; i++)
+      {
+        varOutput [i] = GenerateARA();
+      }
+    }
+    
+    // if PUEO, create PUEO antennas
+    elseif (design == "PUEO")
+    {
+      // determine sections and genes for pueo
+      sections = 1;
+      genes = 9; // Dylan please replace this
+      for(int i=0; i<NPOP; i++)
+      {
+        varOutput [i] = GeneratePUEO();
+      }
+    }
+    // write information to data files
+    DataWrite(varOutput, selected);
+  }
+  //    Initalize(ARA/PUEO)
+  
   //    DataWrite
   // Otherwise:
   //    DataRead
