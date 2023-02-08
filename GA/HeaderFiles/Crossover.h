921753f38a4d3b732342e94bfb8acf7388bb25ac
dylanwells37 <105832162+dylanwells37@users.noreply.github.com>
2023-02-08 17:56:21 -0500

    Update Crossover.h

--- GA/HeaderFiles/Crossover.h
+++ GA/HeaderFiles/Crossover.h
@@ -1,3 +1,16 @@
 #pragma once
+extern int seed = time(NULL);
+extern default_random_engine generator(seed);
+extern int generation;
+extern int population;
+extern int sections;
+extern int genes;
+extern int reproduction_no;
+extern int crossover_no;
+extern int mutation_rate;
+extern int sigma; 
+extern int rank_no; 
+extern int roulette_no;
+extern int tournament_no;
 
 void crossover(vector<vector<vector<float> > > & varInput, vector<vector<vector<float> > > & varOutput, vector<float> fitness, vector<int> P_loc, vector<int> & selected, float roul_percentage, float tour_percentage, float rank_percentage, int crossover_no, int pool_size, int reproduction_no, float M_rate, float sigma);
