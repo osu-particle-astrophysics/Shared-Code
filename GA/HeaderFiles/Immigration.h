87fe9757fb6f725f4c4982947d6783a99991cc3a
dylanwells37 <105832162+dylanwells37@users.noreply.github.com>
2023-02-08 17:57:23 -0500

    Update Immigration.h

--- GA/HeaderFiles/Immigration.h
+++ GA/HeaderFiles/Immigration.h
@@ -1,3 +1,16 @@
 #pragma once
+extern int seed = time(NULL);
+extern default_random_engine generator(seed);
+extern int generation;
+extern int population;
+extern int sections;
+extern int genes;
+extern int reproduction_no;
+extern int crossover_no;
+extern int mutation_rate;
+extern int sigma; 
+extern int rank_no; 
+extern int roulette_no;
+extern int tournament_no;
 
 void immigration(vector<vector<vector<float> > > & varOutput, int reproduction_no, int crossover_no, float max_length, float max_radius, float max_sepration, float max_outer_radius, float max_A, float max_B);
