03a5b32c495c4dcfeada1079825e85aba501fd2b
dylanwells37 <105832162+dylanwells37@users.noreply.github.com>
2023-02-08 17:56:02 -0500

    Update ConstraintARA.h

--- GA/HeaderFiles/ConstraintARA.h
+++ GA/HeaderFiles/ConstraintARA.h
@@ -1,3 +1,16 @@
 #pragma once
+extern int seed = time(NULL);
+extern default_random_engine generator(seed);
+extern int generation;
+extern int population;
+extern int sections;
+extern int genes;
+extern int reproduction_no;
+extern int crossover_no;
+extern int mutation_rate;
+extern int sigma; 
+extern int rank_no; 
+extern int roulette_no;
+extern int tournament_no;
 
 bool ConstraintARA(vector<vector<float> > varInput, int side);
