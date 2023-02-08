a23820a2a6c4a2bbeaa4c99effd72ad4345a5199
dylanwells37 <105832162+dylanwells37@users.noreply.github.com>
2023-02-08 17:15:30 -0500

    Update Crossover.h

--- GA/HeaderFiles/Crossover.h
+++ GA/HeaderFiles/Crossover.h
@@ -1,3 +1,3 @@
 #pragma once
 
-void crossover(vector<vector<vector<float> > > & varInput, vector<vector<vector<float> > > & varOutput, vector<float> fitness, vector<int> P_loc, vector<int> & selected, float roul_percentage, float tour_percentage, float rank_percentage, int crossover_no, int pool_size, int reproduction_no, float M_rate, float sigma)
+void crossover(vector<vector<vector<float> > > & varInput, vector<vector<vector<float> > > & varOutput, vector<float> fitness, vector<int> P_loc, vector<int> & selected, float roul_percentage, float tour_percentage, float rank_percentage, int crossover_no, int pool_size, int reproduction_no, float M_rate, float sigma);
