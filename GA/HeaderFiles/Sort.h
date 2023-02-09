27b0b015cb9309399c865e0990c3302ee683c5e8
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-02-09 16:00:13 -0500

    Update Sort.h

--- GA/HeaderFiles/Sort.h
+++ GA/HeaderFiles/Sort.h
@@ -1,5 +1,18 @@
 #pragma once
 
+extern int seed;
+extern int generation;
+extern int population;
+extern int sections;
+extern int genes;
+extern int reproduction_no;
+extern int crossover_no;
+extern int mutation_rate;
+extern int sigma; 
+extern int rank_no; 
+extern int roulette_no;
+extern int tournament_no;
+
 void Sort(vector<float> & fitness, vector<vector<vector<float> > > & varInput, vector<int> & P_loc)
 {
 	int i,j,x,y;
@@ -14,9 +27,9 @@ void Sort(vector<float> & fitness, vector<vector<vector<float> > > & varInput, v
 		double temp = fitness[i];
 		int T = O_loc[i];
 		vector<vector<vector<float>>> location (1,vector<vector<float> >(NSECTIONS,vector <float>(NVARS, 0.0f)));
-		for(int a = 0; a < NSECTIONS; a++)
+		for(int a = 0; a < sections; a++)
 		{
-			for(int b = 0; b < NVARS; b++)
+			for(int b = 0; b < genes; b++)
 			{
 				location[0][a][b] = varInput[i][a][b];
 				
@@ -26,9 +39,9 @@ void Sort(vector<float> & fitness, vector<vector<vector<float> > > & varInput, v
 		{
 			fitness[j] = fitness[j-1];
 			P_loc[j] = P_loc[j-1];
-			for(x = 0; x < NSECTIONS; x++)
+			for(x = 0; x < sections; x++)
 			{
-				for(y = 0; y < NVARS; y++)
+				for(y = 0; y < genes; y++)
 					{
 						varInput[j][x][y] = varInput[j-1][x][y];
 					}
@@ -36,9 +49,9 @@ void Sort(vector<float> & fitness, vector<vector<vector<float> > > & varInput, v
 		}
 		fitness[j]=temp;
 		P_loc[j] = T;
-		for(int a = 0; a < NSECTIONS; a++)
+		for(int a = 0; a < sections; a++)
                 {
-                        for(int b = 0; b < NVARS; b++)
+                        for(int b = 0; b < genes; b++)
                         {
                                 varInput[j][a][b] = location[0][a][b];
                         }
