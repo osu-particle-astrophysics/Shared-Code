bb38d975cf76f142e38f0524b69ece43b36f1671
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-02-09 16:20:36 -0500

    Update Selection.h

--- GA/HeaderFiles/Selection.h
+++ GA/HeaderFiles/Selection.h
@@ -1,25 +1,38 @@
 #pragma once
 
-void Select(int Opp_no, vector<float> Fitness, int Roul_no, int Rank_no, int Tour_no, int Pool, vector<int> locations)
+extern int seed;
+extern int generation;
+extern int population;
+extern int sections;
+extern int genes;
+extern int reproduction_no;
+extern int crossover_no;
+extern int mutation_rate;
+extern int sigma; 
+extern int rank_no; 
+extern int roulette_no;
+extern int tournament_no;
+
+void Select(int Opp_no, vector<float> Fitness, vector<int> locations)
 {
  // initialize Values
- int Roul_Select = Roul_no/100 * Opp_no;
- int Rank_Select = Rank_no/100 * Opp_no;
- int Tour_Select = Tour_no/100 * Opp_no;
+ int Roul_Select = roulette_no/100 * Opp_no;
+ int Rank_Select = rank_no/100 * Opp_no;
+ int Tour_Select = tournament_no/100 * Opp_no;
  
  // Check to make sure values sum to correct value
  while(Roul_Select + Rank_Select + Tour_Select != Opp_no)
  {
   
-  if(Roul_no/100*Opp_no - Roul_select >= 0.5)
+  if(roulette_no/100*Opp_no - Roul_select >= 0.5)
   {
     Roul_select = Roul_select + 1;
   }
-  elseif(Rank_no/100*Opp_no - Rank_no >= 0.5)
+  elseif(rank_no/100*Opp_no - Rank_no >= 0.5)
   {
     Rank_select = Rank_select + 1;
   }
-  elseif(Tour_no/100*Opp_no - Tour_select >= 0.5)
+  elseif(tournament_no/100*Opp_no - Tour_select >= 0.5)
   {
     Tour_select = Tour_select + 1;
   }
@@ -37,7 +50,7 @@ void Select(int Opp_no, vector<float> Fitness, int Roul_no, int Rank_no, int Tou
  }
  for(int i=0; i>Tour_Select; i++)
  {
-  locations.push_back(Tournament(Fitness, Pool));
+  locations.push_back(Tournament(Fitness));
  }
  
  
