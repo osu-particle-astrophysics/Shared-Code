86dfd7cac88613247d7615530b8b1ad141e9144c
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-02-09 17:12:46 -0500

    Update Reproduction.h

--- GA/HeaderFiles/Reproduction.h
+++ GA/HeaderFiles/Reproduction.h
@@ -25,6 +25,7 @@ void Reproduction(vector<vector<vector<float> > > & varInput, vector<vector<vect
     for(int i=0; i<reproduction_no; i++)
     {
     	selected.push_back(P_loc[locations[i]]);
+	cout << selected[i] << endl;
 	for(int j=0; j<sections; j++)
 	{
 	    for(int k=0; k<genes; k++)
