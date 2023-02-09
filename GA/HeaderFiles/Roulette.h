aa9428e40303c3b113b8698e5dd139c1955de3ab
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-02-09 16:23:06 -0500

    Update Roulette.h

--- GA/HeaderFiles/Roulette.h
+++ GA/HeaderFiles/Roulette.h
@@ -1,5 +1,19 @@
 #pragma once
 
+extern int seed;
+extern default_random_engine generator(seed);
+extern int generation;
+extern int population;
+extern int sections;
+extern int genes;
+extern int reproduction_no;
+extern int crossover_no;
+extern int mutation_rate;
+extern int sigma; 
+extern int rank_no; 
+extern int roulette_no;
+extern int tournament_no;
+
 int Roulette(vector<float> fitness)
 {
   vector<float> probabilities;
