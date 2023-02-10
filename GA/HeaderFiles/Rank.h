70985cf696331018c377257abe1a6fcee7b08975
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-02-10 08:00:58 -0500

    Update Rank.h

--- GA/HeaderFiles/Rank.h
+++ GA/HeaderFiles/Rank.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// Global Variables
 extern int seed;
 extern int generation;
 extern int population;
@@ -15,20 +16,27 @@ extern int tournament_no;
 
 int Rank(vector<float> fitness)
 {
+  // Define Starting parameters
   vector<float> probabilities;
   float sum_npop=0;
+  
+  // get the length of the fitness array
   for(int i=1; i<=fitness.size(); i++)
     {
       sum_npop = sum_npop + i;
     }
+  
+  // Assign probability to each individual
   for(int j=0; j<fitness.size(); j++)
     {
       probabilities.push_back((fitness.size()-j)/(sum_npop));
     }
 
+  // Call a random number 
   uniform_real_distribution<float> choice(0.0, 1.0);
   float select = choice(generator);
 
+  // Grab the individual defined by the random number
   int x=0;
   float probability_sum =0;
   for (int k=0; probability_sum <= select; k++)
@@ -36,5 +44,7 @@ int Rank(vector<float> fitness)
       probability_sum = probability_sum + probabilities[k];
       x=k;
     }
+  
+  // Return the chosen individual
   return(x);
 }
