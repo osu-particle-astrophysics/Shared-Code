209cafcc5928d50901c60d06d6f67b0813833662
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-02-15 17:44:16 -0500

    Update ConstraintARA.h

--- GA/HeaderFiles/ConstraintARA.h
+++ GA/HeaderFiles/ConstraintARA.h
@@ -10,25 +10,40 @@ bool ConstraintARA(float R, float L, float A, float B)
   float vertex = 0.0f;
   float end_point = A*L*L + B*L + R;
   
-  if(A != 0.0){
+  if(A != 0.0)
+  {
     vertex = (R - (B*B)/(4*A));
-  }else{
+  }
+  else
+  {
     vertex = end_point;
   }
-  if(A == 0.0 && max_radius > end_point && end_point >= 0.0){
-    if(R < 0.0 || L < min_length || L > max_length || A < min_coeff || A > max_coeff || B < min_coeff || B > max_coeff){
+  if(A == 0.0 && max_radius > end_point && end_point >= 0.0)
+  {
+    if(R < 0.0 || L < min_length || L > max_length || A < min_coeff || A > max_coeff || B < min_coeff || B > max_coeff)
+    {
       intersect = true;
-    }else{
+    }
+    else
+    {
       intersect = false;
     }
-  }else if (A != 0.0 && max_radius > end_point && end_point >= 0.0 && max_radius > vertex && vertex >= 0.0){
-    if(R < 0.0 || L < min_length || L > max_length || A < min_coeff || A > max_coeff || B < min_coeff || B > max_coeff){
+  }
+  else if (A != 0.0 && max_radius > end_point && end_point >= 0.0 && max_radius > vertex && vertex >= 0.0){
+    if(R < 0.0 || L < min_length || L > max_length || A < min_coeff || A > max_coeff || B < min_coeff || B > max_coeff)
+    {
       intersect = true;
-    }else{
+    }
+    else
+    {
       intersect = false;
     }
-  }else{
+  }
+  else
+  {
     intersect = true;
   }
+  
+  cout << intersect << endl;
   return intersect;
 }
