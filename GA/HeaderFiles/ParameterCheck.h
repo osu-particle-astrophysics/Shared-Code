3dfbd6c7f9a3f92bf487105cad977a0380d561c3
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-03-01 18:14:29 -0500

    Update ParameterCheck.h

--- GA/HeaderFiles/ParameterCheck.h
+++ GA/HeaderFiles/ParameterCheck.h
@@ -15,7 +15,7 @@ extern int rank_no;
 extern int roulette_no;
 extern int tournament_no;
 extern bool termination;
-extern string error_code;
+extern string message;
 
 void ParameterCheck(int argc)
 {
@@ -25,24 +25,24 @@ void ParameterCheck(int argc)
   if (argc != 11)
   {
     termination = true;
-    error_code = "ERROR: Improper amount of arguments";
+    message = "ERROR: Improper amount of arguments";
   }
   
   else if (reproduction_no < 0 || crossover_no < 0 || mutation_rate>100 || mutation_rate<0 || sigma<0 || rank_no <0 || roulette_no < 0 || tournament_no < 0 || crossover_0%2 != 0)
   {
     termination = true;
-    error_code = "ERROR: One or more arguments are of an improper value. For example, values may not be less than zero";
+    message = "ERROR: One or more arguments are of an improper value. For example, values may not be less than zero";
   }
   
   else if (design != "PUEO" || design != "ARA")
   {
     termination = true;
-    error_code = "ERROR: Unknown design. Known designs are PEUO and ARA";
+    message = "ERROR: Unknown design. Known designs are PEUO and ARA";
   }
   
   else if (roulette_no+tournament_no+rank_no > population || reproduction_no + crossover_no > population || roulette_no+tournament_no+rank_no < reproduction_no+crossover_no)
   {
     termination = true;
-    error_code = "ERROR: Sum of selection or opperator parameters is improper.";
+    message = "ERROR: Sum of selection or opperator parameters is improper.";
   }
 }
