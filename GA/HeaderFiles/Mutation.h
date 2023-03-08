60e1ed0a8ade00390bb688eea2e01c2f0daa2ff0
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-03-08 17:33:25 -0500

    Update Mutation.h

--- GA/HeaderFiles/Mutation.h
+++ GA/HeaderFiles/Mutation.h
@@ -71,8 +71,6 @@ void Mutation(vector<vector<vector<float> > > & varOutput)
 				else if (design == "PUEO")
 				{
 					intersect = ConstraintPUEO(temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6]);
-					
-					// cout << temp[0] << ", "<< temp[1] << ", " << temp[2] << ", " << temp[3] << ", " << temp[4] << ", " << temp[5] << ", " << temp[6] << ", " << endl;
 				}
 			}
 			// Save temp values back to the output vector
