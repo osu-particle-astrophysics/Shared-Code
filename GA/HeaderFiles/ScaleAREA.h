cfa907dfd74749104e4427037684c4bc6de16290
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-11 15:52:22 -0400

    Create ScaleAREA.h

---
+++ GA/HeaderFiles/ScaleAREA.h
@@ -0,0 +1,9 @@
+#pragma once
+
+std::vector<std::vector<float> > ScaleAREA(vector<vector<float> > antenna)
+{
+  // Scale the individual to conserve energy
+  
+  // Return scaled individual
+  return antenna;
+}
