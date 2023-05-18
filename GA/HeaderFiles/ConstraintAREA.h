26c2f429e10eeae10313c61b5a6e46bd30aaa931
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-18 15:04:41 -0400

    Update ConstraintAREA.h

--- GA/HeaderFiles/ConstraintAREA.h
+++ GA/HeaderFiles/ConstraintAREA.h
@@ -1,5 +1,5 @@
 #pragma once
-bool ConstraintAREA(vector<vector<float> > antenna)
+bool ConstraintAREA(vector<vector<float> > & antenna)
 {
   bool intersect = true;
   
@@ -8,6 +8,23 @@ bool ConstraintAREA(vector<vector<float> > antenna)
     
   // Use gains to check energy conservation
   // Check if min gain value is negative, if it is, rescale gain coefficents 
+  float Min = 0.0;
+  while (intersect == true)
+  {
+    // Scale the antenna's coefficients
+    antenna = ScaleAREA(antenna, Gains);
+    
+    // Resolve Gains
+    vector<vector<vector<float>>> Gains = SolveGains(antenna);
+    
+    // Double Check that the Min of Gains is not negative
+    Min = FindMin(Gains);
+    if (Min >= 0.0)
+    {
+      intersect = false;
+    }
+  }
   
+  // Return the interesect constraint
   return intersect;
 }
