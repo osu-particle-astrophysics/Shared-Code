810b33798082992bd55a5492fc28390a89d42314
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-22 12:56:32 -0400

    Update New_GA.cpp

--- GA/SourceFiles/New_GA.cpp
+++ GA/SourceFiles/New_GA.cpp
@@ -100,7 +100,7 @@ int main(int argc, char const *argv[])
   // VECTORS
   vector<int> P_loc (population); // Parent locations vector
   vector<float> fitness (population, 0.0f); // stores fitness score
-  vector<int> selected (population);
+  vector<int> selected;
   
   // Check the design and prepare input/output vectors
   if (design == "ARA")
