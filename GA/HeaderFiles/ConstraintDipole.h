adf1799929ac9b7f9ec7631240c06250ed7a4d27
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-05-30 16:15:26 -0400

    Update ConstraintDipole.h

--- GA/HeaderFiles/ConstraintDipole.h
+++ GA/HeaderFiles/ConstraintDipole.h
@@ -3,10 +3,12 @@ bool ConstraintDipole(float R, float L)
 {
     // Set variables
     bool intersect = true;
-    float max_radius = 1.0;
-    float min_radius = 0.0;
-    float max_length = 1.0;
-    float min_length = 0.0;
+
+    // distances in cm
+    float max_radius = 10.0;
+    float min_radius = 0.25;
+    float max_length = 10000.0;
+    float min_length = 200.0;
 
     // Check to see if antenna meets requirements
     if (R > max_radius || R < min_radius  || L > max_length || L < min_length)
