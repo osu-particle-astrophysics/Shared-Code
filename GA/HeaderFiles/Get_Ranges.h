b8f593509227bd5b930a38f4f3fa958c61293b76
Ryantdebolt <62964601+Ryantdebolt@users.noreply.github.com>
2023-06-06 01:56:27 -0400

    Update Get_Ranges.h

--- GA/HeaderFiles/Get_Ranges.h
+++ GA/HeaderFiles/Get_Ranges.h
@@ -8,30 +8,31 @@ extern int genes;
 void Get_Ranges(float& variable_max, float& variable_min, int gene)
 {
   // Establish max and min range vectors for each design
-
+  vector<float> max_value;
+  vector<float> min_value;
   if (design == "ARA")
   {
     // ARA ranges
-    vector<float> max_value{7.5, 140.0, 1.0, 1.0};
-    vector<float> min_value{0, 10.0, -1.0, -1.0};
+    max_value = {7.5, 140.0, 1.0, 1.0};
+    min_value = {0, 10.0, -1.0, -1.0};
   }
   else if (design == "PUEO")
   {
     // PUEO ranges
-    vector<float> max_value{50.0, 175, 50.0, 50.0, 175.0, 175.0, 12.25};
-    vector<float> min_value{0.0, 75.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    max_value = {50.0, 175, 50.0, 50.0, 175.0, 175.0, 12.25};
+    min_value = {0.0, 75.0, 0.0, 0.0, 0.0, 0.0, 0.0};
   }
   else if (design == "AREA")
   {
     // AREA ranges
-    vector<float> max_value{5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 };
-    vector<float> min_value{-5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0};
+    max_value = {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0 };
+    min_value = {-5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0, -5.0};
   }
   else if (design == "Symmetric Dipole" || design == "Asymmetric Dipole")
   {
     // Dipole ranges
-    vector<float> max_value{ 10.0, 10000.0 };
-    vector<float> min_value{ 0.25, 200.0 };
+    max_value = { 10.0, 10000.0 };
+    min_value = { 0.25, 200.0 };
   }
 
   // Find value for the gene that is being mutated. 
