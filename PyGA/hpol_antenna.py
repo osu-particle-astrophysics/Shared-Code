675116cbd1b99562a8a03f2bdf0574edb62e3f2d
jacobweiler <zed8drift@gmail.com>
2024-08-01 10:42:34 -0400

    Added Support for Ferrite Rod Dimensions
    - Added in ferrite rods to HPOL antenna

--- PyGA/hpol_antenna.py
+++ PyGA/hpol_antenna.py
@@ -19,6 +19,10 @@ class HPOLAntenna:
         self.arc_length_plate_max = 10.0
         self.height_min = 0.5
         self.height_max = 10.0
+        self.ferrite_height_min = 1.0
+        self.ferrite_height_max = 12.0
+        self.ferrite_radius_min = 0.5
+        self.ferrite_radius_max = 3.0
     
     def initialize(self):
         '''Initialize the genes of the HPOL antenna. 
@@ -29,13 +33,43 @@ class HPOLAntenna:
         radius = random.uniform(self.radius_min, self.radius_max)
         arc_length_plate = random.uniform(self.arc_length_plate_min, self.arc_length_plate_max)
         height = random.uniform(self.height_min, self.height_max)
+        ferrite_height = random.uniform(self.ferrite_height_min, self.ferrite_height_max)
+        ferrite_radius = random.uniform(self.ferrite_radius_min, self.ferrite_radius_max)
         
-        self.genes = [num_plates, radius, arc_length_plate, height]
+        self.genes = [num_plates, radius, arc_length_plate, height, ferrite_height, ferrite_radius]
     
     def save_as_comparison(self, filename):
         '''save the current genes as a comparison file.'''
         filepath = Path(f"comparisons/{filename}.txt")
         np.savetxt(filepath, self.genes)
+    
+    def check_genes(self):
+        '''Check if the genes are valid. 
+        Return False if the genes are invalid.'''
+        
+        # Load genes
+        (num_plates, radius, arc_length_plate, height, ferrite_height, ferrite_radius) = self.genes
+
+        # Variables
+        valid_design = False
+        
+        
+        # Run checks
+        if (not (self.radius_min <= radius <= self.radius_max) or 
+            not (self.height_min <= height <= self.height_max)):
+            valid_design = False
+        elif not (self.num_plates_min <= num_plates <= self.num_plates_max):
+            valid_design = False
+        elif not (self.arc_length_plate_min <= arc_length_plate <= self.arc_length_plate_max):
+            valid_design = False
+        elif not (self.ferrite_height_min <= ferrite_height <= self.ferrite_height_max):
+            valid_design = False
+        elif not (self.ferrite_radius_min <= ferrite_radius <= self.ferrite_radius_max):
+            valid_design = False
+        else:
+            valid_design = True
+
+        return valid_design
     
     
     def __str__(self) -> str:
