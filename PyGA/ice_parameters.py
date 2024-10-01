35035746577564b4573ca2b2cad1efc9a1b856bc
Machtay <amachtay@proton.me>
2024-10-01 11:34:43 -0400

    Adding branch for fitting to ice parameters. Making a new class called IceParameters.

---
+++ PyGA/ice_parameters.py
@@ -0,0 +1,114 @@
+import random
+
+from pathlib import Path
+
+import numpy as np
+
+class IceParameters:
+    
+    def __init__(self, genes=None):
+        self.genes = genes
+        self.fitness = 0.0
+        self.true_fitness = 0.0
+        self.MAX_PHI = 2*np.pi ## Rotation about z-axis
+        self.MAX_THETA = np.pi ## Rotation about y-axis
+        self.MAX_PSI = 2*np.pi ## Rotation about z-axis
+				self.MAX_CROSS_POL = np.pi ## Initial cross-polarization angle
+        
+    
+    def initialize(self):
+        '''Initialize the genes of the horn antenna. 
+        The genes are: [side_length, height, x_0, y_0, z_f, y_f, 
+        beta, trpzd_length, trpzd_height]'''
+        
+        valid_design = False
+        while valid_design == False:
+            # Generate random values
+            side_length = random.uniform(0.0, self.MAX_S)
+            height = random.uniform(self.MIN_H, self.MAX_H)
+            x_0 = random.uniform(0.0, side_length)
+            y_0 = random.uniform(0.0, x_0)
+            z_f = random.uniform(0.0, height)
+            y_f = random.uniform(0.0, z_f)
+            beta = random.uniform((4.0/30.0) * z_f, 7.0 * z_f) / 100.0
+            trpzd_length = random.uniform(0.0, y_0)
+            trpzd_height = random.uniform(0.0, x_0)
+            
+            self.genes = [side_length, height, 
+                          x_0, y_0, z_f, y_f, 
+                          beta, trpzd_length, 
+                          trpzd_height]
+            
+            valid_design = self.check_genes()
+        
+        
+    
+    def check_genes(self):
+        '''Check if the genes are valid. 
+        Return False if the genes are invalid.'''
+        
+        # Load genes
+        (side_length, height, x_0, y_0, z_f, y_f, 
+         beta, trpzd_length, trpzd_height) = self.genes
+
+        # Variables
+        valid_design = False
+        x_f = side_length
+        
+        # Calculate trapezoid intersection value
+        trpzd_intersect = (trpzd_length * x_0 + trpzd_height * y_0 -
+            x_0 * y_0) / (trpzd_height + trpzd_length - y_0)
+        
+        # Run checks
+        if (not (0 <= side_length <= self.MAX_S) or 
+            not (self.MIN_H <= height <= self.MAX_H)):
+            valid_design = False
+        elif not (0 <= x_0 <= x_f):
+            valid_design = False
+        elif not (0 <= y_0 <= min(z_f, x_0)):
+            valid_design = False
+        elif not (0 <= y_f <= z_f):
+            valid_design = False
+        elif not (0 <= z_f <= height):
+            valid_design = False
+            
+        # Check if trapezoids touch
+        elif x_0 - trpzd_height < trpzd_intersect < x_0:
+            valid_design = False
+        elif ((4.0 / 30.0) * z_f > (beta * 100) or 
+              (7 * z_f) < (beta * 100) or beta > 2):
+            valid_design = False
+        else:
+            valid_design = True
+
+        return valid_design
+    
+    
+    def evaluate_fitness(self, comparison):
+        '''Calculate the euclidean distance between the genes
+        of the horn antenna and the comparison genes.'''
+        
+        euclidean_distance = 0.0
+        gene_count = len(self.genes)
+        for i in range(gene_count):
+            numerator = (self.genes[i] - comparison[i]) ** 2
+            denominator = (self.genes[i] + comparison[i]) ** 2
+            euclidean_distance += numerator / denominator
+            
+        normalized_distance = euclidean_distance / (2 * gene_count)
+        
+        normalized_distance = normalized_distance ** 0.5
+        
+        self.fitness = 1.0 - normalized_distance
+    
+    
+    def save_as_comparison(self, filename):
+        '''save the current genes as a comparison file.'''
+        filepath = Path(f"comparisons/{filename}.txt")
+        np.savetxt(filepath, self.genes)
+    
+    
+    def __str__(self) -> str:
+        '''Return a string representation of the antenna's genes.'''
+        return str(self.genes)
+        
